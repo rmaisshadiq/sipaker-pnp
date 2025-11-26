35e3b71839aa720080c121c549554e882e92c85d
Rafi Maisshadiq <74971414+rmaisshadiq@users.noreply.github.com>
2025-11-26 20:03:54 +0700

    feat: inisialisasi imagekit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "m.media-amazon.com",
+        port: "",
+        protocol: "https",
+      },
+      {
+        hostname: "ik.imagekit.io",
+        port: "",
+        protocol: "https"
+      }
+    ]
+  },
   env: {
     databaseUrl: process.env.DATABASE_URL,
   }
